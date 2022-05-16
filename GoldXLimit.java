9ce4cef4e660bdedba2baa3a94cab47dc35d74b1
Ravikumar <ravikumar.ee@gmail.com>
2022-05-16 09:19:09 +0530

    Update GoldXLimit.java

--- GoldXLimit.java
+++ GoldXLimit.java
@@ -1,1 +1,16 @@
 
+package com.iamtrk.scratchpad.goldx;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GoldXLimit {
+    double price;
+    GoldXLimit next;
+    GoldXLimit prev;
+    List<GoldXOrder> orderList = new LinkedList<>();
+
+    public GoldXLimit(double price) {
+        this.price = price;
+    }
+}
