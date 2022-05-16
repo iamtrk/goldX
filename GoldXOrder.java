88da532a7d972bfe7d51eab30bce79d5b09b0f1b
Ravikumar <ravikumar.ee@gmail.com>
2022-05-16 09:18:42 +0530

    Update GoldXOrder.java

--- GoldXOrder.java
+++ GoldXOrder.java
@@ -1,1 +1,18 @@
 
+package com.iamtrk.scratchpad.goldx;
+
+public class GoldXOrder {
+    String userId;
+    OrderAction action;
+    long time;
+    int weight;
+    double price;
+
+    public GoldXOrder(String userId, OrderAction action, long time, int weight, double price) {
+        this.userId = userId;
+        this.action = action;
+        this.time = time;
+        this.weight = weight;
+        this.price = price;
+    }
+}
